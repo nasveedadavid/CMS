74d4ddf15a85210b30f5039aa58c246e0a85505f
nasshjacob <65451254+nasshjacob@users.noreply.github.com>
2020-11-10 11:06:42 +0530

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+<?php echo $_POST["name"]; ?><br>
+Your username is: admin111 and ppassword is 1234
+</body>
+</html>
